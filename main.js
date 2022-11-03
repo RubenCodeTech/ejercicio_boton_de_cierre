c41369c77b17ad755fb45c688b5d1b0db5850c24
RubenCodeTech <pzgz.ruben@gmail.com>
2022-11-03 16:26:30 +0100

    all exercises

---
+++ main.js
@@ -0,0 +1,58 @@
+// Hay una lista de mensajes.
+// Usa JavaScript para agregar un botón de cierre en la esquina superior derecha de cada mensaje.
+
+
+// ownHider.onclick = function(){
+//     ownHider.hidden = true;
+// } 
+
+// button1.onclick = function(){
+//     firstSection.hidden = true
+// }
+// button2.onclick = function(){
+//     SecondSection.hidden = true
+// }
+// button3.onclick = function(){
+//     thirdSection.hidden = true
+// }
+
+// con una variable que haga querySelectorAll y un bucle for que haga lo mismo.
+
+// let allButtons = [...document.querySelectorAll('input')]
+
+// for (i = 0; i < allButtons.length; i++) {
+
+//     let a = allButtons[i]
+//     if (a == allButtons[0]) {
+//         button1.onclick = function () {
+//             firstSection.hidden = true
+
+//         }
+//     } else if (a == allButtons[1]) {
+//         button2.onclick = function () {
+//             SecondSection.hidden = true
+//         }
+//     } else if (a == allButtons[2]) {
+//         button3.onclick = function () {
+//             thirdSection.hidden = true
+//         }
+//     }
+// }
+
+// let allButtons = [...document.querySelectorAll('input')]
+// let mySections = document.getElementsByClassName('sections');
+// for(let x of  allButtons){
+//     x.onclick = function() {
+//         mySections.hidden = true
+//     }
+// }
+
+
+
+// let allButtons = [...document.querySelectorAll('input')]
+// let mySections = document.getElementsByClassName('sections');
+// for(let x=0; x< allButtons.length; x++){
+//     allButtons[x].onclick = function(){
+//         mySections.hidden = true
+//     }
+// }
